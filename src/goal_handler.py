22fbafe2d9aa9ed41cce78ebce4d6c22c9390b10
Ian Loefgren <ian.loefgren@colorado.edu>
2016-06-13 10:06:58 -0600

    updated robot_control.py and renamed file to goal_handler.py

--- src/goal_handler.py
+++ src/goal_handler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Written by: Ian Loefgren
+Written by Ian Loefgren
 May 2016
 
 The goalHandler class publishes goal poses to the navigation stack via /move_base_simple/goal.
@@ -21,11 +21,11 @@ avoidance_type: '-s' or '-b'
 	Choose between 'secondary' or 'blocked' avoidance actions, which determine
 	how the robot deals with blocked goals. Defaults to none, which results in only
 	avoidance decisions by the navigation stack local planner, with no high-level decisions by
-	the POMDP policy. 	
+	the POMDP policy.
 
 Output
 -----------
-None	
+Goal poses sent to the ROS navigation stack.
 
 """
 
@@ -52,7 +52,6 @@ import tf
 from pose import Pose
 
 
-
 class goalHandler(object):
 
 	def __init__(self, filename,avoidance_type):
@@ -67,18 +66,18 @@ class goalHandler(object):
 		self.tf_exception_wrapper()
 		self.goal_point = self.pose._pose
 		self.avoidance = sys.argv[2]
-		
 		self.pub = rospy.Publisher('/deckard/move_base_simple/goal',PoseStamped,queue_size=10)
 		rospy.sleep(1) #<>TODO: figure out why the hell this works --> solves issue where robot would not move on initialization
 		rospy.Subscriber('/deckard/move_base/status',GoalStatusArray,self.callback)
 		print("initial position: " + str(self.pose._pose))
 
 	def tf_exception_wrapper(self):
-		#waits for transforms to become available and handles interim exceptions
+		"""waits for transforms to become available and handles interim exceptions
+		"""
 		tries = 0
 		while not self.tf_exist and tries < 10:
-			try: 
-				self.pose.tf_update()	
+			try:
+				self.pose.tf_update()
 				self.tf_exist = True
 				#self.current_position = self.pose._pose
 				#self.goal_point = self.pose._pose
@@ -87,24 +86,30 @@ class goalHandler(object):
 				self.tf_exist = False
 				print("\nError!")
 				print(error)
-				print("Waiting for transforms to become available. Will retry 10 times.") 
+				print("Waiting for transforms to become available. Will retry 10 times.")
 				print("Try: " + str(tries) + "  Retrying in 2 seconds.\n")
-				rospy.sleep(2)		
+				rospy.sleep(2)
 
 	def callback(self,msg):
+		"""callback function that runs when messages are published to /move_base/status.
+		The function updates its knowledge of its position using tf data, then
+		checks if the robot is stuck and sends the appropriate goal pose.
+		"""
+
 		self.pose.tf_update()
 		#self.last_position = self.pose._pose
 		if self.is_stuck():
 			self.send_goal(True)
-			rospy.sleep(5)
+			while not self.is_at_goal():
+				continue
 		else:
-			self.send_goal()	
-		#self.is_at_goal()		
+			self.send_goal()
+		#self.is_at_goal()
 		#print("status: " + str(self.current_status) + "\tcheck: " + str(self.current_status==3) + "\tcurrent position: " + str(self.pose._pose))
 		#if self.current_status == 3:
 		#self.stuck_count = self.stuck_buffer
 			#self.current_status = 1
-		rospy.sleep(1)	
+		rospy.sleep(1)
 
 	def is_at_goal(self):
 		"""checks if robot has arrived at its goal pose
@@ -116,9 +121,10 @@ class goalHandler(object):
 			#print("X goal diff: " + str(abs(self.goal_point[0] - self.current_position[0])) + "\tY goal diff: " + str(abs(self.goal_point[1] - self.current_position[1])))
 			if abs(self.goal_point[0] - self.pose._pose[0]) < tol and abs(self.goal_point[1] - self.pose._pose[1]) < tol:
 				self.current_status = 3
+				return True
 		except TypeError:
 			print("Goal pose does not yet exist!")
-			self.current_status = 3	
+			self.current_status = 3
 
 	def is_stuck(self):
 		"""re-sends goal pose if robot is mentally or physically stuck for self.stuck_buffer number of iterations
@@ -131,16 +137,15 @@ class goalHandler(object):
 		self.stuck_count = self.stuck_buffer
 		self.stuck_distance = math.sqrt(((self.pose._pose[0] - self.last_position[0]) ** 2) + ((self.pose._pose[1] - self.last_position[1]) ** 2))
 		self.last_position = self.pose._pose
-		print(self.stuck_distance)		
+		print(self.stuck_distance)
 		if self.stuck_distance < 0.5:
 			print("Robot stuck; resending goal.")
-			#self.send_goal(True)
 			return True
 		else:
-			return False	
+			return False
 
 	def get_new_goal(self,current_position,stuck_flag): #<>TODO: refine stuck vs blocked, as the robot can get stuck without technically beiing blocked
-		"""get new goal pose from module
+		"""get new goal pose from discretePolicyTranslator module
 		"""
 
 		point = current_position
@@ -151,33 +156,35 @@ class goalHandler(object):
 		if stuck_flag and self.avoidance == '-b':
 			return self.dpt.getNextPose(point,stuck_flag)
 		elif stuck_flag and self.avoidance == '-s':
-			return self.dpt.getNextPose(point,False,stuck_flag)	
+			return self.dpt.getNextPose(point,False,stuck_flag)
 		else:
 			return self.dpt.getNextPose(point)
 
 	def send_goal(self,stuck_flag=False):
-		"""get and send new goal pose
+		"""get and send new goal pose. Returns false without sending pose if pose to send
+		is the same as the current pose and the robot is not stuck (meaning it is enroute
+		to that pose)
 		"""
-		
+
 		new_pose = self.get_new_goal(self.pose._pose,stuck_flag)
 		if self.goal_point == new_pose and not stuck_flag:
 			return False
 		else:
 			self.goal_point = new_pose
 			print("sent goal: " + str(self.goal_point))
-		
+
 		new_goal = PoseStamped()
 		new_goal.pose.position.x = self.goal_point[0]
 		new_goal.pose.position.y = self.goal_point[1]
 		new_goal.pose.position.z = self.goal_point[2]
 		theta = self.goal_point[3]
 		print("first theta: " + str(theta))
-		if theta > 120 + abs(self.pose._pose[2]) and stuck_flag: 
+		if theta > 120 + abs(self.pose._pose[2]) and stuck_flag:
 			theta = 90
 			print("second theta: " + str(theta))
 
 		if self.goal_point == [2,2,0,0]: #<>TODO: Fix this gross hack, it makes puppies cry
-			theta = 180	
+			theta = 180
 		quat = tf.transformations.quaternion_from_euler(0,0,np.deg2rad(theta))
 		new_goal.pose.orientation.x = quat[0]
 		new_goal.pose.orientation.y = quat[1]
