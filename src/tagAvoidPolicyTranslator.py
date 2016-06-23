46bb1b238b348f3cf295cea496544a8d57a1756b
Ian Loefgren <ian.loefgren@colorado.edu>
2016-06-23 09:54:17 -0600

    rounding bug in tagAvoidPolicyTranslator.py fixed, gitignore updated, goal_handler.py updated though code cleanup still necessary. Two-robot tag avoid confirmed working.

--- src/tagAvoidPolicyTranslator.py
+++ src/tagAvoidPolicyTranslator.py
@@ -39,232 +39,233 @@ class tagAvoidPolicyTranslator(discretePolicyTranslator):
 		if(fileName == None):
 			self.alphas = self.readAlphas("fakealphas1.txt");
 		else:
-			self.alphas = self.readAlphas(fileName); 
+			self.alphas = self.readAlphas(fileName);
 		if(isinstance(self.alphas[0],float)):
 			self.numStates = len(self.alphas)-1;
 		else:
 			self.numStates = len(self.alphas[0])-1;
-		self.goalX= 0; 
-		self.goalY = 0;  
-		self.hardware = hardware; 
+		self.goalX= 0;
+		self.goalY = 0;
+		self.hardware = hardware;
 
 
 	def normalize(self,a):
 
-		Suma = sum(a); 
+		Suma = sum(a);
 		for i in range(0,len(a)):
-			a[i] = float(a[i])/Suma; 
-		return a; 
+			a[i] = float(a[i])/Suma;
+		return a;
 
 	def aToxy(self,a):
 		x1 = int(a/1000);
-		y1 = int((a-x1*1000)/100); 
-		x2 = int((a-x1*1000-y1*100)/10); 
+		y1 = int((a-x1*1000)/100);
+		x2 = int((a-x1*1000-y1*100)/10);
 		y2 = int((a-x1*1000 - y1*100 - x2*10));
 
-		return [x1,y1,x2,y2]; 
+		return [x1,y1,x2,y2];
 
 	def xyToa(self,x1,y1,x2,y2):
-		return x1*1000+y1*100+x2*10+y2	
+		return x1*1000+y1*100+x2*10+y2
 
 	def xyToa(self,c):
-		return c[0]*1000+c[1]*100+c[2]*10+c[3]; 
+		return c[0]*1000+c[1]*100+c[2]*10+c[3];
 
 
 	def distance(self,x1,y1,x2,y2):
-		a = (x1-x2)*(x1-x2); 
-		b = (y1-y2)*(y1-y2); 
-		return sqrt(a+b); 
+		a = (x1-x2)*(x1-x2);
+		b = (y1-y2)*(y1-y2);
+		return sqrt(a+b);
 
 	def fakeBelief(self,x1,y1,x2,y2):
-		arr = [0]*self.numStates; 
-		a = self.xyToa([x1,y1,x2,y2]); 
-		arr[a] = 1; 
-		return arr;  
+		arr = [0]*self.numStates;
+		a = self.xyToa([x1,y1,x2,y2]);
+		arr[a] = 1;
+		return arr;
 
 
 	def printMap(self,cx,cy,rx,ry):
-		map1 = ""; 
+		map1 = "";
 		for i in range(9,-1,-1):
 			for j in range(0,10):
 				if(j == cx and i == cy):
-					map1 = map1+'O'; 
+					map1 = map1+'O';
 				elif(j==rx and i == ry):
-					map1 = map1+'X'; 
+					map1 = map1+'X';
 				else:
-					map1 = map1+"-"; 
-			map1 = map1+"\n"; 
-		print(map1); 
+					map1 = map1+"-";
+			map1 = map1+"\n";
+		print(map1);
 
 
 	def getNextCopPose(self,copPose,robberPose,secondary=False):
-
+		print('getting next cop pose...')
 
 		if(self.hardware):
-			cx = int(copPose[0]*2); 
-			cy = int(copPose[1]*2); 
-			rx = int(robberPose[0]*2); 
-			ry = int(robberPose[1]*2); 
+			cx = int(round(copPose[0]*2));
+			cy = int(round(copPose[1]*2));
+			rx = int(round(robberPose[0]*2));
+			ry = int(round(robberPose[1]*2));
 		else:
-			cx = int(copPose[0]); 
-			cy = int(copPose[1]); 
-			rx = int(robberPose[0]); 
-			ry = int(robberPose[1]); 
+			cx = int(copPose[0]);
+			cy = int(copPose[1]);
+			rx = int(robberPose[0]);
+			ry = int(robberPose[1]);
 
-		a = [cx,cy,rx,ry]; 
+		a = [cx,cy,rx,ry];
 
-		belief = self.fakeBelief(cx,cy,rx,ry); 
+		belief = self.fakeBelief(cx,cy,rx,ry);
 
- 
+
 		if(secondary == False):
-			action = self.getAction(self.alphas,belief); 
+			action = self.getAction(self.alphas,belief);
 		else:
 			action = self.getSecondaryAction(self.alphas,belief);
 
 
-		
-		orient = 0.0; 
 
-		#TODO: The up and down are switched. Maybe change that back? 
+		orient = 0.0;
+
+		#TODO: The up and down are switched. Maybe change that back?
 
 		if(action == 0):
-			destX = cx-1; 
-			destY = cy; 
-			actVerb = "Left"; 
-			orient = 180.0; 
+			destX = cx-1;
+			destY = cy;
+			actVerb = "Left";
+			orient = 180.0;
 		elif(action == 1):
-			destX = cx+1; 
-			destY = cy; 
-			actVerb = "Right"; 
-			orient = 0.0; 
+			destX = cx+1;
+			destY = cy;
+			actVerb = "Right";
+			orient = 0.0;
 		elif(action == 3):
-			destX = cx; 
-			destY = cy+1; 
+			destX = cx;
+			destY = cy+1;
 			actVerb = "Up";
-			orient = 90.0; 
+			orient = 90.0;
 		elif(action == 2):
-			destX = cx; 
-			destY = cy-1; 
+			destX = cx;
+			destY = cy-1;
 			actVerb = "Down";
-			orient = -90.0; 
+			orient = -90.0;
 		else:
-			destX = cx; 
+			destX = cx;
 			destY = cy;
 			actVerb = "Wait";
 
-		
+
 		if(self.hardware):
-			return [float(destX)/2,float(destY)/2,0.0,orient]; 
+			return [float(destX)/2,float(destY)/2,0.0,orient];
 		else:
-			return [destX,destY,0,orient]; 
+			return [destX,destY,0,orient];
 
 
 	def getNextRobberPose(self,copPose,robberPose):
+		print('getting next robber pose...')
 		if(self.hardware):
-			cx = int(copPose[0]*2); 
-			cy = int(copPose[1]*2); 
-			rx = int(robberPose[0]*2); 
-			ry = int(robberPose[1]*2); 
+			cx = int(round(copPose[0]*2));
+			cy = int(round(copPose[1]*2));
+			rx = int(round(robberPose[0]*2));
+			ry = int(round(robberPose[1]*2)); 
 		else:
-			cx = int(copPose[0]); 
-			cy = int(copPose[1]); 
-			rx = int(robberPose[0]); 
-			ry = int(robberPose[1]); 
+			cx = int(copPose[0]);
+			cy = int(copPose[1]);
+			rx = int(robberPose[0]);
+			ry = int(robberPose[1]);
 
-		weights = []; 
-		dests = []; 
-		holds = []; 
+		weights = [];
+		dests = [];
+		holds = [];
 
 		if(cx-rx <= 0 and rx < 9):
 			dests += [[rx+1,ry,1]];
-			weights += [.4];  
-			holds += [1]; 
+			weights += [.4];
+			holds += [1];
 		elif(cx-rx>=0 and rx > 0):
 			dests += [[rx-1,ry,0]]
-			weights += [.4]; 
-			holds +=[0]; 
+			weights += [.4];
+			holds +=[0];
 		if(cy-ry <= 0 and ry < 9):
-			dests += [[rx,ry+1,2]]; 
-			weights += [.4]; 
-			holds+=[2]; 
+			dests += [[rx,ry+1,2]];
+			weights += [.4];
+			holds+=[2];
 		elif(cy-ry >= 0 and ry > 0):
 			dests += [[rx,ry-1,3]];
-			weights += [.4]; 
+			weights += [.4];
 			holds +=[3]
 
-		dests += [[rx,ry,4]]; 
-		weights += [.2]; 
-		holds += [4]; 
+		dests += [[rx,ry,4]];
+		weights += [.2];
+		holds += [4];
 
 
-		weights = self.normalize(weights); 
+		weights = self.normalize(weights);
 
 		tmp = np.random.choice(holds,p=weights);
-		dest = dests[holds.index(tmp)];  
-		orient = 0.0; 
+		dest = dests[holds.index(tmp)];
+		orient = 0.0;
 		if(dest[2] == 0):
-			orient = 180.0; 
+			orient = 180.0;
 		elif(dest[2] == 1):
-			orient == 0.0; 
+			orient == 0.0;
 		elif(dest[2] == 2):
-			orient = 90.0; 
+			orient = 90.0;
 		elif(dest[2] == 3):
-			orient = -90.0; 
-		else: 
-			orient = 0.0; 
+			orient = -90.0;
+		else:
+			orient = 0.0;
 
 		if(self.hardware):
-			return [float(dest[0])/2,float(dest[1])/2,0.0,orient]; 
+			return [float(dest[0])/2,float(dest[1])/2,0.0,orient];
 		else:
-			return [dest[0],dest[1],0,orient]; 
+			return [dest[0],dest[1],0,orient];
 
 
 	def simulate(self):
-		cx = 1; 
-		cy = 1; 
-		rx = 5; 
-		ry = 5; 
+		cx = 1;
+		cy = 1;
+		rx = 5;
+		ry = 5;
 
-		self.copsx = []; 
-		self.copsy = []; 
-		self.robsx = []; 
-		self.robsy = []; 
+		self.copsx = [];
+		self.copsy = [];
+		self.robsx = [];
+		self.robsy = [];
 
 
-		action = 0; 
+		action = 0;
 
-		flag = False; 
-		count = 0; 
+		flag = False;
+		count = 0;
 		while(cx != rx or cy != ry):
 			a = self.getNextRobberPose([cx,cy],[rx,ry]);
-			b = self.getNextCopPose([cx,cy],[rx,ry]); 
+			b = self.getNextCopPose([cx,cy],[rx,ry]);
 
-			count+=1; 
+			count+=1;
 
-			rx = a[0]; 
-			ry = a[1]; 
-			cx = b[0]; 
-			cy = b[1]; 
+			rx = a[0];
+			ry = a[1];
+			cx = b[0];
+			cy = b[1];
 
 			if(cx < 0 or cy < 0 or ry < 0 or rx < 0 or cx > 9 or cy > 9 or rx > 9 or ry > 9):
-				print("Error: Robot out of bounds"); 
-				flag = True; 
-				break; 
+				print("Error: Robot out of bounds");
+				flag = True;
+				break;
 
-			self.copsx += [cx]; 
-			self.copsy += [cy]; 
-			self.robsx += [rx]; 
-			self.robsy += [ry]; 
+			self.copsx += [cx];
+			self.copsy += [cy];
+			self.robsx += [rx];
+			self.robsy += [ry];
 
-			print("Cop position: "); 
-			print(cx,cy);  
-			print("Robber position: "); 
-			print(rx,ry); 
-			self.printMap(cx,cy,rx,ry); 
-			print(""); 
-			#plt.scatter([cx,rx],[cy,ry]); 
+			print("Cop position: ");
+			print(cx,cy);
+			print("Robber position: ");
+			print(rx,ry);
+			self.printMap(cx,cy,rx,ry);
+			print("");
+			#plt.scatter([cx,rx],[cy,ry]);
 			#plt.axis([-.5,9.5,-.5,9.5])
-			#plt.show(); 
+			#plt.show();
 
 
 		print("Congratulations!! The cop caught the robber in: " + str(count) + " moves.")
@@ -277,18 +278,18 @@ def init():
 
 
 def animate(i):
-	global copsx; 
+	global copsx;
 	global copsy;
 	global robsx;
-	global robsy; 
+	global robsy;
 
-	#x = cops[0][0]; 
-	#y = cops[0][1]; 
+	#x = cops[0][0];
+	#y = cops[0][1];
 	if(len(copsx) > 1):
 		line.set_data(copsx,copsy)
 
-		copsx = copsx[1:]; 
-		copsy = copsy[1:]; 
+		copsx = copsx[1:];
+		copsy = copsy[1:];
 		return line,
 '''
 
@@ -296,17 +297,17 @@ def animate(i):
 
 if __name__ == "__main__":
 
-	#file = "../policies/tagAvoidEmpty100.txt"; 
+	#file = "../policies/tagAvoidEmpty100.txt";
 	file = "../policies/tagAvoidWalls100.txt";
-	#file = "../policies/tagRandomWalls100.txt"; 
-	t = tagAvoidPolicyTranslator(file); 
+	#file = "../policies/tagRandomWalls100.txt";
+	t = tagAvoidPolicyTranslator(file);
 	t.simulate()
 
 
-	copsx = t.copsx; 
-	copsy = t.copsy; 
-	robsx = t.robsx; 
-	robsy = t.robsy; 
+	copsx = t.copsx;
+	copsy = t.copsy;
+	robsx = t.robsx;
+	robsy = t.robsy;
 
 
 
@@ -318,8 +319,5 @@ if __name__ == "__main__":
 
 
 
-	anim = animation.FuncAnimation(fig, animate, init_func=init,frames=100, interval=500,save_count = 100, blit=False)	 
-	plt.show(); 		
-
-
-
+	#anim = animation.FuncAnimation(fig, animate, init_func=init,frames=100, interval=500,save_count = 100, blit=False)
+	#plt.show();
