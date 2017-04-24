b268f1b1daed6ef86ad5d2d746b765362bf316a3
luke <clburks9@gmail.com>
2017-04-23 21:12:46 -0600

    online methods investigation

---
+++ Investigations/Invest16_OnlinePOMDP/testOnlinePOMDP.py
@@ -0,0 +1,21 @@
+from sys import path
+
+path.append('../../src/');
+from gaussianMixtures import GM, Gaussian 
+
+
+#Search
+def search(h):
+
+	numIter = 10; 
+	for i in range(0,numIter):
+		if(len(h)==0):
+			#draw from uniform
+		else:
+			#draw from belief
+		#simulate
+
+	return 
+
+
+
